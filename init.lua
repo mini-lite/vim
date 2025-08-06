d32b54cbe35e54bbf6c9d99c67560b00acec2493
Salah <salaheddine.ghamri@yahoo.com>
2025-08-06 17:50:43 +0200

    more motions

--- init.lua
+++ init.lua
@@ -1,13 +1,14 @@
 -- Author: Salah Eddine Ghamri
 
 -- Change log-----------------------------------------------------------------
--- TODO: add disable vim config
+-- TODO: track vim commands using a state
 -- TODO: normal mode is working outside doc view
 -- TODO: start adding important navigations
 -- TODO: we need to set space as leader or adapt if user want space as leader
 -- TDOD: add basic commands
 -- TODO: create an option if vim global or only to doc views
 -- TODO: let user extend commands
+-- TODO: add disable vim config
 
 -- DONE: show message notifying the change of state
 -- DONE: give the command line a name
@@ -23,6 +24,8 @@ local common = require "core.common"
 local command = require "core.command"
 local keymap = require "core.keymap"
 local style = require "core.style"
+local translate = require "core.doc.translate"
+local DocView = require "core.docview"
 
 local command_line = require "plugins.command-line"
 command_line.set_item_name("status:vim")
@@ -30,15 +33,44 @@ command_line.add_status_item()
 command_line.minimal_status_view = true -- only item will show
 
 local vim = {
-  mode = "normal", -- visual, command
-  registers = { ['"'] = "" },  -- unnamed register
+  mode = "normal", -- normal, visual, command
+  registers = { ['"'] = "" },
   command_buffer = "",
   search_query = "",
-  motion_count = "",
   command_map = {},
+  operators = {},
+  motions = {},
   normal_keys = {},
   visual_keys = {},
 }
+
+-- state to track commands
+local state = {
+  count = 1,
+  operator = nil,
+  motion = nil,
+  register = nil,
+}
+
+-- helper
+local function echo(fmt, ...)
+  local text = string.format(fmt, ...)
+  command_line.show_message({text}, 1)
+end
+
+-- normalize count, default 1
+local function get_count()
+  return tonumber(state.count) or 1
+end
+
+-- reset state helper
+local function reset_state()
+  state.count = 1
+  state.operator = nil
+  state.motion = nil
+  state.register = nil
+  vim.command_buffer = "" -- debug
+end
 
 -- set instance command line
 local instance_command = command_line.new()
@@ -55,12 +87,6 @@ backward_search:set_prompt("?")
 -- keys
 local pressed = {}
 
--- Combined keys
--- g, t/T, f/F, numbers
-local pending_key = nil
-local pending_nbr = nil
-local go_to_line_nbr = 1
-
 -- keys that can modify text
 local modifying_keys = {
   ["return"] = true,
@@ -71,43 +97,60 @@ local modifying_keys = {
   ["space"] = true
 }
 
-local function handle_normal(text)
-  -- f, F: Find commands (character navigation).
-  -- t, T: Till/To commands (stop before/after character).
-  if pending_key then
-    local combo = pending_key .. text
-    go_to_line_nbr = tonumber(pending_nbr) or 1
-    pending_key = nil
-    pending_nbr = nil
-    local fn = vim.normal_keys[combo]
-    if fn then fn() end
+-- accumulate keys
+local pending = ""
+local prev_dig = false
+
+local function handle_normal(key)
+  -- accumulate count digits only if no operator pending
+  if not state.operator and (key:match("[1-9]") or (key == "0" and prev_dig )) then
+    prev_dig = true
+    state.count = (state.count == 1 and "" or tostring(state.count)) .. key
     return true
   end
 
-  -- g: Goto/Prefix command (for extended motions).
-  if text == "g" then
-    pending_key = text
-    return true -- wait for next key
+  -- here numbers already filtered
+  pending = pending .. key
+  prev_dig = false
+
+  -- try operator first
+  if vim.operators[pending] then
+    state.operator = vim.operators[pending]
+    pending = ""
+    return true
   end
 
-  if tonumber(text) then
-      if pending_nbr then
-         pending_nbr = pending_nbr .. text
-      else
-         pending_nbr = text
-      end
-      return true
+  -- try motion
+  if vim.motions[pending] then
+    local count = get_count()
+    if state.operator then
+      state.operator(count, vim.motions[pending])
+    else
+      vim.motions[pending](count)
+    end
+    pending = ""
+    reset_state()
+    return true
+  end
+
+  -- normal key 
+  if vim.normal_keys and vim.normal_keys[key] then
+    vim.normal_keys[key]()
+    reset_state()
+    return true
+  end
+
+  -- if pending gets too long, reset
+  if #pending > 2 then
+    pending = ""
+    reset_state()
+    return true
   end
   
-  local fn = vim.normal_keys[text]
-  if fn then
-      fn()
-  end
-  return true --block 
+  return true
 end
 
 local function handle_insert(text)
-  -- TODO: define other keys
   return false
 end
 
@@ -136,6 +179,7 @@ function core.on_event(type, a, ...)
 
         if a == "escape" then
             vim.set_mode("normal")
+            reset_state()
             instance_command:cancel_command()
             return true --block
         end
@@ -171,7 +215,7 @@ function vim.set_mode(m)
 end
 
 -- TODO: we need a real parser
-local vim_command_map = {
+local vim_ex_commands = {
   w  = { action = "doc:save", desc = "Save file" },
   q  = { action = "core:quit", desc = "Quit editor" },
   qa = { action = "core:quit-all", desc = "Quit all" },
@@ -180,7 +224,7 @@ local vim_command_map = {
 }
 
 function vim.run_command(cmd)
-  local entry = vim_command_map[cmd]
+  local entry = vim_ex_commands[cmd]
   if entry then
     command.perform(entry.action)
   else
@@ -190,7 +234,7 @@ end
 
 function vim.get_suggests(input)
   local suggestions = {}
-  for name, entry in pairs(vim_command_map) do
+  for name, entry in pairs(vim_ex_commands) do
     if name:find("^" .. input) then
       table.insert(suggestions, {
         name = name,
@@ -234,6 +278,276 @@ function vim.search_word_under_cursor()
   if word then vim.search_forward(word) end
 end
 
+-- operators
+vim.operators = {
+  -- ["d"]
+  -- ["gu"]
+  -- ["gU"]
+  -- ["<"]
+  -- [">"]
+  -- ["c"]
+  ["y"] = function(count, motion)
+    local doc = core.active_view.doc
+    -- use motion(count) to get range or lines to yank
+    -- Here simplified: yank current line for demo
+    local line = doc.lines[doc.cursor.line]
+    vim.registers['"'] = line
+    core.log("Yanked line")
+  end,
+  ["p"] = function(count, motion)
+    local doc = core.active_view.doc
+    local text = vim.registers['"'] or ""
+    doc:insert(doc.cursor, text .. "\n")
+  end,
+}
+
+-- motions ----------------------------------------------------
+local config = require "core.config"
+
+--local function is_non_word(char)
+--  return config.non_word_chars:find(char, nil, true)
+--end
+
+-- helper text objects
+local classify
+do
+  local map = {}
+  for i = 0, 255 do
+    map[i] = "punct"
+  end
+  for i = string.byte("0"), string.byte("9") do
+    map[i] = "word"
+  end
+  for i = string.byte("A"), string.byte("Z") do
+    map[i] = "word"
+  end
+  for i = string.byte("a"), string.byte("z") do
+    map[i] = "word"
+  end
+
+  -- include as word
+  map[string.byte("_")] = "word"
+  map[string.byte("-")] = "word"
+
+  for _, c in ipairs { 9, 10, 11, 12, 13, 32 } do
+    map[c] = "space"
+  end
+
+  function classify(char)
+    if not char or char == "" then return "eof" end
+    return map[string.byte(char)] or "punct"
+  end
+end
+
+local translations = {
+  ["previous-char"]        = translate,
+  ["next-char"]            = translate,
+  ["next-word-start"] = function(doc, line, col)
+    local char = doc:get_char(line, col)
+    local ctype = classify(char)
+  
+    while true do
+      line, col = doc:position_offset(line, col, 1)
+      local next_char = doc:get_char(line, col)
+      local next_type = classify(next_char)
+      if next_type ~= ctype then
+        ctype = next_type
+        break
+      end
+    end
+  
+    while ctype == "space" do
+      line, col = doc:position_offset(line, col, 1)
+      local next_char = doc:get_char(line, col)
+      ctype = classify(next_char)
+    end
+  
+    return line, col
+  end,
+  ["previous-word-start"]  = function(doc, line, col)
+    local char = doc:get_char(line, col)
+    local ctype = classify(char)
+  
+    while true do
+      line, col = doc:position_offset(line, col, -1)
+      local next_char = doc:get_char(line, col)
+      local next_type = classify(next_char)
+      if next_type ~= ctype then
+        ctype = next_type
+        break
+      end
+    end
+  
+    while ctype == "space" do
+      line, col = doc:position_offset(line, col, -1)
+      local next_char = doc:get_char(line, col)
+      ctype = classify(next_char)
+    end
+  
+    return translate.start_of_word(doc, line, col)
+  end,
+  ["start-of-word"] = function(doc, line, col)
+    local ctype = classify(doc:get_char(line, col))
+    while true do
+      local prev_line, prev_col = doc:position_offset(line, col, -1)
+      if prev_line == line and prev_col == col then break end 
+      local prev_type = classify(doc:get_char(prev_line, prev_col))
+      if prev_type ~= ctype then break end
+      line, col = prev_line, prev_col
+    end
+    return line, col
+  end,
+  
+  ["end-of-word"] = function(doc, line, col)
+    local ctype = classify(doc:get_char(line, col))
+    while true do
+      local next_line, next_col = doc:position_offset(line, col, 1)
+      if next_line == line and next_col == col then break end 
+      local next_type = classify(doc:get_char(next_line, next_col))
+      if next_type ~= ctype then break end
+      line, col = next_line, next_col
+    end
+    return line, col
+  end,
+  ["start-of-line"]        = translate,
+  ["start-of-indentation"] = translate,
+  ["end-of-line"]          = translate,
+  ["start-of-doc"]         = translate,
+  ["end-of-doc"]           = translate,
+  ["next-line"]            = DocView.translate,
+  ["previous-line"]        = DocView.translate,
+  ["next-page"]            = DocView.translate,
+  ["previous-page"]        = DocView.translate,
+  ["next-block-end"]       = translate,
+  ["previous-block-start"] = translate,
+}
+
+local function get_motion_fn(name)
+  local obj = translations[name]
+  if type(obj) == "function" then
+    return obj
+  elseif obj then
+    return obj[name:gsub("-", "_")]
+  end
+  return nil
+end
+
+
+vim.motions = {
+  ["h"] = function(count)
+    local dv = core.active_view
+    local fn = get_motion_fn("previous-char")
+    if not fn then return end
+    for _ = 1, count do
+      for idx, line1, col1, line2, col2 in dv.doc:get_selections(true) do
+        if line1 ~= line2 or col1 ~= col2 then
+          dv.doc:set_selections(idx, line1, col1)
+        else
+          dv.doc:move_to_cursor(idx, fn)
+        end
+      end
+      dv.doc:merge_cursors()
+    end
+  end,
+
+  ["l"] = function(count)
+    local dv = core.active_view
+    local fn = get_motion_fn("next-char")
+    if not fn then return end
+    for _ = 1, count do
+      for idx, line1, col1, line2, col2 in dv.doc:get_selections(true) do
+        if line1 ~= line2 or col1 ~= col2 then
+          dv.doc:set_selections(idx, line2, col2)
+        else
+          dv.doc:move_to_cursor(idx, fn)
+        end
+      end
+      dv.doc:merge_cursors()
+    end
+  end,
+
+  ["j"] = function(count)
+    local dv = core.active_view
+    local fn = get_motion_fn("next-line")
+    if not fn then return end
+    for _ = 1, count do
+      dv.doc:move_to(fn, dv)
+    end
+  end,
+
+  ["k"] = function(count)
+    local dv = core.active_view
+    local fn = get_motion_fn("previous-line")
+    if not fn then return end
+    for _ = 1, count do
+      dv.doc:move_to(fn, dv)
+    end
+  end,
+
+  ["w"] = function(count)
+    local dv = core.active_view
+    count = count or 1
+    local next_word_start = get_motion_fn("next-word-start")
+    for _ = 1, count do
+        dv.doc:move_to(next_word_start, dv)
+    end
+  end,
+
+  ["b"] = function(count)
+    local dv = core.active_view
+    local previous_word_start = get_motion_fn("previous-word-start")
+    count = count or 1
+    for _ = 1, count do
+        dv.doc:move_to(previous_word_start, dv)
+    end
+  end,
+
+  ["e"] = function(count)
+    local dv = core.active_view
+    count = count or 1
+    local next_word_start = get_motion_fn("next-word-start")
+    for _ = 1, count do
+        dv.doc:move_to(next_word_start, dv)
+        dv.doc:move_to(translations["end-of-word"], dv)
+    end
+  end,
+
+  ["0"] = function()
+    local doc = core.active_view.doc
+    local line, _ = doc:get_selection()
+    doc:set_selection(line, 1)
+  end,
+
+  ["^"] = function()
+    local doc = core.active_view.doc
+    local line, _ = doc:get_selection()
+    local text = doc.lines[line] or ""
+    local first_non_ws = text:find("%S") or 1
+    doc:set_selection(line, first_non_ws)
+  end,
+
+  ["$"] = function()
+    local doc = core.active_view.doc
+    local line, _ = doc:get_selection()
+    local text = doc.lines[line] or ""
+    local last_col = #text + 1
+    doc:set_selection(line, last_col)
+  end,
+
+  ["gg"] = function(count)
+    local doc = core.active_view.doc
+    local line = count or 1
+    doc:set_selection(line, 1)
+  end,
+
+  ["G"] = function()
+    local doc = core.active_view.doc
+    local last_line = #doc.lines
+    local last_col = #(doc.lines[last_line] or "") + 1
+    doc:set_selection(last_line, last_col)
+  end,
+}
+
 -- normal keymaps
 vim.normal_keys = {
   [":"] = function() end,
@@ -243,31 +557,6 @@ vim.normal_keys = {
 
   ["i"] = function()
     vim.set_mode("insert")
-  end,
-
-  ["y"] = function()
-    local doc = core.active_view.doc
-    local line = doc.lines[doc.cursor.line]
-    vim.registers['"'] = line
-    core.log("Yanked line")
-  end,
-
-  ["p"] = function()
-    local doc = core.active_view.doc
-    local text = vim.registers['"'] or ""
-    doc:insert(doc.cursor, text .. "\n")
-  end,
-
-  ["gg"] = function()
-    local doc = core.active_view.doc
-    doc:set_selection(go_to_line_nbr, 1)
-  end,
-
-  ["G"] = function()
-    local doc = core.active_view.doc
-    local last_line = #doc.lines
-    local last_col = #(doc.lines[last_line] or "") + 1
-    doc:set_selection(last_line, last_col)
   end,
 }
 
